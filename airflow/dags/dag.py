2ffe7def1eb9caf22501f79861825d1e8fd28c66
Khuong Nguyen Ba <nguyenbakhuong64@gmail.com>
2025-02-23 09:30:25 +0100

    Add environment variables to separate file & added implementation for loading data to trusted zone

--- airflow/dags/dag.py
+++ airflow/dags/dag.py
@@ -4,6 +4,8 @@ import os
 import json
 import boto3
 import logging
+from dotenv import load_dotenv
+import ast
 
 from airflow import DAG
 from airflow.decorators import task
@@ -11,20 +13,24 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator   
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
+from airflow.models import Variable
 
 from scripts.util.retrieve_data import DataRetrieverOverAPI
 
 # Define the logger
 LOGGER = logging.getLogger(__name__)
 
+# Establish environment variables
+load_dotenv()
+
 # Define the environment variables
-DAWUM_API_URL = "https://api.dawum.de/"
-MINIO_ENDPOINT_URL = "http://minio:9000/"
-MINIO_AWS_ACCESS_KEY_ID = "vKNa0lEmBxjcO8l1EaL0"
-MINIO_AWS_SECRET_ACCESS_KEY = "C95bTTeoaywJF95JwIUZhZaCA1xomN95BcgAeGUH"
+DAWUM_API_URL = os.getenv("DAWUM_API_URL")
+MINIO_AWS_ACCESS_KEY_ID = os.getenv("MINIO_AWS_ACCESS_KEY_ID")
+MINIO_AWS_SECRET_ACCESS_KEY = os.getenv("MINIO_AWS_SECRET_ACCESS_KEY")
+MINIO_ENDPOINT_URL = os.getenv("MINIO_ENDPOINT_URL")
+POSTGRES_CONN_ID = os.getenv("POSTGRES_CONN_ID")
+DATA_ENTRIES = ast.literal_eval(Variable.get("DATA_ENTRIES", default_var="[]"))
+SPARK_CONN_ID = os.getenv("SPARK_CONN_ID")
 
 # Establish connection to Minio
 S3_CLIENT = boto3.client('s3', 
@@ -44,32 +50,17 @@ with DAG(
 
     @task
     def extract() -> None:
-    # Extract data from api
+        # Setup and fetch data entries to retrieve from the api
         dawum_api = DataRetrieverOverAPI(DAWUM_API_URL)
-        json_filenames = ['database.json', 
-                        'parliaments.json', 
-                        'institutes.json', 
-                        'taskers.json', 
-                        'methods.json', 
-                        'parties.json', 
-                        'surveys.json'
-                        ]
-        dawums = [dawum_api.data['Database'], 
-                dawum_api.data['Parliaments'], 
-                dawum_api.data['Institutes'], 
-                dawum_api.data['Taskers'], 
-                dawum_api.data['Methods'], 
-                dawum_api.data['Parties'], 
-                dawum_api.data['Surveys']
-                ]
-        
-        LOGGER.info("[DONE] - Extracted data from API")
-
+        json_filenames = list(map(lambda x: x + ".json", DATA_ENTRIES))
+        data_entries = list(map(lambda x: x.capitalize(), DATA_ENTRIES))
+    
         # Write data to minio's bucket
-        for i, file in enumerate(dawums):
-            data = json.dumps(file, indent=4)   
+        for i, data_entry in enumerate(data_entries):
+            data = json.dumps(dawum_api.data[data_entry], indent=4)   
             S3_CLIENT.put_object(Bucket="election-data", Key="landing/" + json_filenames[i], Body=data)
 
+        print(os.environ)
         LOGGER.info("[DONE] - Written data to Minio's bucket")
 
 
@@ -99,7 +90,7 @@ with DAG(
 
     @task
     def create_table():
-        pg_hook = PostgresHook(postgres_conn_id='postgres_conn')
+        pg_hook = PostgresHook(postgres_conn_id=POSTGRES_CONN_ID)
         conn = pg_hook.get_conn()
         cursor = conn.cursor()
         sql = """
@@ -128,7 +119,9 @@ with DAG(
 
     extract_task = extract()
 
-    spark_task = spark_job(task_id='spark_job_convert_database',  spark_job_path='/data/convert_database.py', connection_id='spark_conn')
+    spark_task = spark_job(task_id='spark_job_convert_database',  
+                           spark_job_path='/data/convert_database.py', 
+                           connection_id=SPARK_CONN_ID)
 
     load_task = load()
 
--- airflow/dags/scripts/spark_jobs/convert_database.py
+++ airflow/dags/scripts/spark_jobs/convert_database.py
@@ -1,26 +1,155 @@
+import os
+from dotenv import load_dotenv
+import boto3
+import ast
+from io import StringIO
+import json
+
 import pyspark.sql.functions as F
 from pyspark.sql import SparkSession
 from pyspark.context import SparkContext
 from pyspark.conf import SparkConf
 
+# Load the environment variables
+load_dotenv()
+
+
+# Read the environment variables
+DAWUM_API_URL = os.getenv("DAWUM_API_URL")
+MINIO_AWS_ACCESS_KEY_ID = os.getenv("MINIO_AWS_ACCESS_KEY_ID")
+MINIO_AWS_SECRET_ACCESS_KEY = os.getenv("MINIO_AWS_SECRET_ACCESS_KEY")
+MINIO_ENDPOINT_URL = os.getenv("MINIO_ENDPOINT_URL")
+POSTGRES_CONN_ID = os.getenv("POSTGRES_CONN_ID")
+DATA_ENTRIES = ast.literal_eval(os.getenv("DATA_ENTRIES"))
+SPARK_CONN_ID = os.getenv("SPARK_CONN_ID")
+
+# Establish connection to Minio
+S3_CLIENT = boto3.client('s3', 
+                         endpoint_url=MINIO_ENDPOINT_URL, 
+                         aws_access_key_id=MINIO_AWS_ACCESS_KEY_ID, 
+                         aws_secret_access_key=MINIO_AWS_SECRET_ACCESS_KEY)
+
+
 def main():
+    # Establish spark session
     spark = SparkSession.builder \
                         .appName("Spark Basics") \
                         .getOrCreate()
+    
+    json_filenames = list(map(lambda x: x + ".json", DATA_ENTRIES))
+    dfs = [] # list to store the dataframes
 
+    # Load parliaments data
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[1])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_parliaments = rdd.map(lambda x: (x[0], x[1]['Name'], x[1]['Shortcut'], x[1]['Election'])).toDF(["parliament_id", "parliament_name", "parliament_shortcut", "parliament_election"])
+    df_parliaments.show()
+    dfs.append(df_parliaments)
 
-    data = [("Alice", 25, "New York"), ("Bob", 30, "San Francisco"),
-            ("Cathy", 22, "Chicago"), ("David", 35, "New York"),
-            ("Eve", 29, "San Francisco")]
-    columns = ["Name", "Age", "City"]
+    # Load institutes data                
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[2])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_institutes = rdd.map(lambda x: (x[0], x[1]["Name"])).toDF(["institute_id", "institute_name"])
+    df_institutes.show()
+    dfs.append(df_institutes)
 
-    df = spark.createDataFrame(data, columns)
+    # Load taskers data
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[3])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_taskers = rdd.map(lambda x: (x[0], x[1]["Name"])).toDF(["tasker_id", "tasker_name"])
+    df_taskers.show()
+    dfs.append(df_taskers)
 
-    df.show()
+    # Load methods data
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[4])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_methods = rdd.map(lambda x: (x[0], x[1]["Name"])).toDF(["method_id", "method_name"])
+    df_methods.show()
+    dfs.append(df_methods)
 
-    print("Filtering data")
+    # Load parties data
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[5])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_parties = rdd.map(lambda x: (x[0], x[1]["Name"], x[1]["Shortcut"])).toDF(["party_id", "party_name", "party_shortcut"])
+    df_parties.show()
+    dfs.append(df_parties)
 
+    # Load surveys data
+    response = S3_CLIENT.get_object(Bucket="election-data", 
+                                    Key="landing/" + json_filenames[6])
+    data = response['Body'].read()
+    json_data = data.decode('utf-8') 
+    json_data = json.loads(json_data)
+    rdd = spark.sparkContext.parallelize(json_data.items())
+    df_surveys = rdd.map(lambda x: (x[0], 
+                                    x[1]["Parliament_ID"],
+                                    x[1]["Institute_ID"],
+                                    x[1]["Tasker_ID"],
+                                    x[1]["Method_ID"],
+                                    x[1]["Date"], 
+                                    x[1]["Survey_Period"]["Date_Start"], 
+                                    x[1]["Survey_Period"]["Date_End"],
+                                    x[1]["Surveyed_Persons"],
+                                    x[1]["Results"])) \
+                        .toDF(["survey_id", 
+                               "parliament_id",
+                               "institute_id",
+                               "tasker_id",
+                               "method_id",
+                               "survey_publish_date",
+                               "survey_start_date", 
+                               "survey_end_date",
+                               "total_surveyees",
+                               "results"])
+    df_surveys = df_surveys.select(F.col("survey_id"), 
+                                F.col("parliament_id"),
+                                F.col("institute_id"),
+                                F.col("tasker_id"),
+                                F.col("method_id"),
+                                F.explode(df_surveys.results).alias("party_id", "survey_result_by_percent"),
+                                F.col("survey_publish_date"),
+                                F.col("survey_start_date"), 
+                                F.col("survey_end_date"),
+                                F.col("total_surveyees") 
+                                )
+    df_surveys = df_surveys.fillna({"survey_result_by_percent": 0})
+    df_surveys.show()
+    dfs.append(df_surveys)
+
+    # Write the data to the trusted zone
+    csv_filenames = list(map(lambda x: x + ".csv", DATA_ENTRIES))
+    csv_buffer = StringIO()
+    for i, df in enumerate(dfs): 
+        df = df.toPandas()
+        df.to_csv(csv_buffer, index=False)
+        csv_content = csv_buffer.getvalue()  
+        S3_CLIENT.put_object(Bucket="election-data", 
+                             Key="trusted/" + csv_filenames[i], 
+                             Body=csv_content.encode('utf-8'),
+                             ContentType="text/csv")
+            
     spark.stop()
+
 
 if __name__ == "__main__":
     main()
